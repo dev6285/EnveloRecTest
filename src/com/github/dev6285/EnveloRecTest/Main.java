51e7476b1bc7ca808bf645a47a5faf6102d02028
Alek <alek.pluta@gmail.com>
2022-10-11 14:09:10 +0200

    Repo init.

---
+++ src/com/github/dev6285/EnveloRecTest/Main.java
@@ -0,0 +1,9 @@
+package com.github.dev6285.EnveloRecTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi!\n" +
+                "Welcome to my \"Akademia Envelo w PKP Informatyka\" test.");
+    }
+}
