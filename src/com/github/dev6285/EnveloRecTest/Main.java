5bf412b3ee4a69d7ff678d7c765ce8705d55154d
Alek <alek.pluta@gmail.com>
2022-10-11 16:41:19 +0200

    Added Introduction and commands summary for the "UI".

--- src/com/github/dev6285/EnveloRecTest/Main.java
+++ src/com/github/dev6285/EnveloRecTest/Main.java
@@ -3,7 +3,9 @@ package com.github.dev6285.EnveloRecTest;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi!\n" +
-                "Welcome to my \"Akademia Envelo w PKP Informatyka\" test.");
+        WelcomeMsg welcomeMsg = new WelcomeMsg();
+        welcomeMsg.sayHello();                      // Introduction and commands summary.
+
+
     }
 }
---
+++ src/com/github/dev6285/EnveloRecTest/WelcomeMsg.java
@@ -0,0 +1,14 @@
+package com.github.dev6285.EnveloRecTest;
+
+public class WelcomeMsg {
+    final String welcomeMsg = "==============================================================\n" +
+            "Hi!\n" +
+            "Welcome to my \"Akademia Envelo w PKP Informatyka\" test.\n" +
+            "Here you'll find the task#3 \"API Kanye Rest\" console app.\n" +
+            "Type \"NEXT\" for a different quote.\n" +
+            "Type \":q!\" to exit.\n" +
+            "==============================================================\n\n";
+    public void sayHello() {
+        System.out.println(welcomeMsg);
+    }
+}
