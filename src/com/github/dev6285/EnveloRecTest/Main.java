0c6f890b360cdd8c911b639ebdd968d44611459e
Alek <alek.pluta@gmail.com>
2022-10-13 11:50:07 +0200

    Code cleaning.

--- src/com/github/dev6285/EnveloRecTest/Main.java
+++ src/com/github/dev6285/EnveloRecTest/Main.java
@@ -1,9 +1,5 @@
 package com.github.dev6285.EnveloRecTest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
     public static void main(String[] args) {
