9d38b0e3a2b8f859eb1161da3f2fe5c32de6b0f9
Alek <alek.pluta@gmail.com>
2022-10-13 13:28:24 +0200

    Code cleaning.

--- src/com/github/dev6285/EnveloRecTest/WelcomeMsg.java
+++ src/com/github/dev6285/EnveloRecTest/WelcomeMsg.java
@@ -7,10 +7,8 @@ public class WelcomeMsg {
             "Here you'll find the task#3 \"API Kanye Rest\" console app.\n" +
             "Type \"NEXT\" for a different quote.\n" +
             "Type \":q!\" to exit.\n" +
-            "==============================================================\n\n";
+            "==============================================================\n";
 
-    /*  This could be a getter, but this way we can add some data validation, like different msg for different users,
-     or msg depending on the current date etc. */
     public void sayHello() {
         System.out.println(welcomeMsg);
     }
